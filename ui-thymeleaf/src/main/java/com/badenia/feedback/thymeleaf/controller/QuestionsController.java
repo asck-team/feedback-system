330a5c28ec9155b7f93c31069c03926ea1265c39
andreisava <andreisava@192.168.0.88>
2018-12-23 22:51:24 +0100

    get new order from ui to save
    - get the new order of question in controller QuestionsController,
    method updateOrderQuestion

--- ui-thymeleaf/src/main/java/com/badenia/feedback/thymeleaf/controller/QuestionsController.java
+++ ui-thymeleaf/src/main/java/com/badenia/feedback/thymeleaf/controller/QuestionsController.java
@@ -2,6 +2,8 @@ package com.badenia.feedback.thymeleaf.controller;
 
 import java.util.List;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +17,8 @@ public class QuestionsController extends AbstractController {
 	private static final String MODEL_ATTR_EVENTID = "eventId";
 	private static final String MODEL_ATTR_QUESTIONS = "questions";
 	private static final String UI_TEMPLATE_QUESTIONS = "questions";
+	
+	private static final Logger LOGGER = LogManager.getLogger(QuestionsController.class);
 
 	@GetMapping(path = {"/questions"}, params = MODEL_ATTR_EVENTID)
 	public String index(Model model, @RequestParam(required = true, name = MODEL_ATTR_EVENTID) Long eventId) {
@@ -28,6 +32,14 @@ public class QuestionsController extends AbstractController {
 	public String deleteQuestion(@RequestParam(MODEL_ATTR_EVENTID) Long eventId, @RequestParam("questionId") Long questionId, Model model) {
 		getFeedbackService().deleteQuestion(eventId, questionId);
 		return index(model, eventId);
+	}
+	
+	@GetMapping("/updateOrderQuestion")
+	public String updateOrderQuestion(@RequestParam(MODEL_ATTR_EVENTID) Long eventId, @RequestParam("questionId") Long questionId, @RequestParam("order") int order, Model model) {
+		LOGGER.info("Order aktualisieren!!!!!!!!");
+		LOGGER.info("Eventid: {}, QuestionID: {}, Order: {}", eventId, questionId, order);
+		//TODO Call ServiceMethod updateOrderToQuestion
+		return "redirect:/questions?" + MODEL_ATTR_EVENTID + "=" + eventId;
 	}
 	
 }
