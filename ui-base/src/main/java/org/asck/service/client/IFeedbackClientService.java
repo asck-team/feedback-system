cf2511e467bb97ec5a37801d6dfa0c4308c02faa
andreisava <andreisava@192.168.0.88>
2018-12-27 11:30:47 +0100

    rebase develop to feature formAnswer
    - resolve conflicts

---
+++ ui-base/src/main/java/org/asck/service/client/IFeedbackClientService.java
@@ -0,0 +1,36 @@
+package org.asck.service.client;
+
+import java.util.List;
+
+import org.asck.service.client.model.Answer;
+import org.asck.service.client.model.Event;
+import org.asck.service.client.model.Option;
+import org.asck.service.client.model.Question;
+
+public interface IFeedbackClientService {
+	
+	List<Event> leseAlleEvents();
+	
+	Event saveEvent(Event event);
+	
+	Event getEventById(Long id);
+	
+	List<Question> leseAlleFragenZuEvent(Long eventId);
+
+	List<Option> leseAlleOptionenZuEinerFrage(Long eventId, Long questionId);
+	
+	Question readQuestion(Long eventId, Long questionId);
+	
+	Question saveQuestion(Long eventId, Question question);
+	
+	List<String> readAllSupportedQuestionTypes();
+	
+	void deleteEvent(Long eventId);
+	
+	void deleteQuestion(Long eventId, Long questionId);
+	
+	Answer saveAnswer(Answer answer);
+	
+	
+
+}
