f9e523540a6703a79ad36c940909c9163b686248
andreisava <andreisava@192.168.0.88>
2018-12-27 15:30:50 +0100

    resolve conflicts after merge

--- ui-base/src/main/java/org/asck/service/client/model/Question.java
+++ ui-base/src/main/java/org/asck/service/client/model/Question.java
@@ -20,29 +20,6 @@ public class Question {
 	
 	private String questionType;
 	
-}
-package org.asck.service.client.model;
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-@Builder
-@JsonIgnoreProperties(ignoreUnknown = true)
-public class Question {
-
-	private Long id;
-	
-	private String questionName;
-	
-	private String questionType;
-	
 	private int order;
 	
 }
