8c78856d2d915af1c1b8a266788e54c61d0bf31b
andreisava <andreisava@192.168.0.88>
2018-12-28 23:46:44 +0100

    change name of field

--- ui-thymeleaf/src/main/java/org/asck/controller/QrCodeController.java
+++ ui-thymeleaf/src/main/java/org/asck/controller/QrCodeController.java
@@ -22,14 +22,14 @@ import com.google.zxing.qrcode.QRCodeWriter;
 public class QrCodeController extends AbstractController {
 	
 	@Value("${ip.adress.qr.code:127.0.0.1}") 
-	private String IP_ADRESS; 
+	private String ipAdress; 
 	
 	private static final Logger LOGGER = LogManager.getLogger(NewEventController.class);
 
 	@GetMapping("/qrCode")
 	public String generateQrCode(@RequestParam("eventId") Long eventId, Model model) {
-		LOGGER.info("IP Adress: {}", IP_ADRESS);
-		byte[] qrCore = createAsByteArray("http://"+ IP_ADRESS + ":8081/answer?eventId=" + eventId);
+		LOGGER.info("IP Adress: {}", ipAdress);
+		byte[] qrCore = createAsByteArray("http://"+ ipAdress + ":8081/answer?eventId=" + eventId);
 		String qrCodeString = Base64.getEncoder().encodeToString(qrCore); 
 		model.addAttribute("qrCode", qrCodeString);
 		return "qrCode";
