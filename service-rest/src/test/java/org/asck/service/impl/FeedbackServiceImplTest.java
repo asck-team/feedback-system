4c1b77c2f9fcbe2c18a97062b198e7a8fa0183ea
Constantin Krüger <Constantinidis@web.de>
2018-12-29 02:23:36 +0100

    fix sonar findings and add tests

--- service-rest/src/test/java/org/asck/service/impl/FeedbackServiceImplTest.java
+++ service-rest/src/test/java/org/asck/service/impl/FeedbackServiceImplTest.java
@@ -13,6 +13,7 @@ import org.asck.repository.QuestionTypeRepository;
 import org.asck.repository.model.QuestionTableModel;
 import org.asck.service.model.Question;
 import org.asck.service.model.QuestionType;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -91,6 +92,74 @@ public class FeedbackServiceImplTest {
 		
 		when(questionRepository.findAllByEventIdOrderByOrder(1L)).thenReturn(Arrays.asList(QuestionTableModel.builder().id(1L).eventId(1L).order(1).questionTitle("questionName").questionTypeId(QuestionType.FIVE_SMILEYS.getDbId()).build()));
 		QuestionTableModel questionTableModel2Save = QuestionTableModel.builder().id(-1L).eventId(1L).order(2).questionTitle("questionName").questionTypeId(QuestionType.FIVE_SMILEYS.getDbId()).build();
+		when(questionRepository.save(questionTableModel2Save)).thenReturn(QuestionTableModel.builder().id(2L).build());
+		
+		
+		underTest.saveQuestion(1L, Question.builder().id(-1L).order(2).questionName("questionName").questionType(QuestionType.FIVE_SMILEYS).build());
+	}
+	
+	/**
+	 * Test method for
+	 * {@link org.asck.service.impl.FeedbackServiceImpl#saveQuestion(java.lang.Long, org.asck.service.model.Question)}.
+	 */
+	@Test
+	@Ignore
+	public void testSaveQuestion_OneQuestionsExistsQuestionToSaveIsTheSameButHasNewOrder_SaveQuestionWithOrder1() throws Exception {
+		
+		when(questionRepository.findAllByEventIdOrderByOrder(1L)).thenReturn(Arrays.asList(QuestionTableModel.builder().id(1L).eventId(1L).order(1).questionTitle("questionName").questionTypeId(QuestionType.FIVE_SMILEYS.getDbId()).build()));
+		QuestionTableModel questionTableModel2Save = QuestionTableModel.builder().id(1L).eventId(1L).order(1).questionTitle("questionName").questionTypeId(QuestionType.FIVE_SMILEYS.getDbId()).build();
+		when(questionRepository.save(questionTableModel2Save)).thenReturn(questionTableModel2Save);
+		
+		
+		underTest.saveQuestion(1L, Question.builder().id(1L).order(2).questionName("questionName").questionType(QuestionType.FIVE_SMILEYS).build());
+	}
+	
+	/**
+	 * Test method for
+	 * {@link org.asck.service.impl.FeedbackServiceImpl#saveQuestion(java.lang.Long, org.asck.service.model.Question)}.
+	 */
+	@Test
+	public void testSaveQuestion_OneQuestionsExistsQuestionToSaveHaveOrderLowerThenZero_SaveQuestionWithOrder2() throws Exception {
+		
+		when(questionRepository.findAllByEventIdOrderByOrder(1L)).thenReturn(Arrays.asList(QuestionTableModel.builder().id(1L).eventId(1L).order(1).questionTitle("questionName").questionTypeId(QuestionType.FIVE_SMILEYS.getDbId()).build()));
+		QuestionTableModel questionTableModel2Save = QuestionTableModel.builder().id(-1L).eventId(1L).order(2).questionTitle("questionName").questionTypeId(QuestionType.FIVE_SMILEYS.getDbId()).build();
+		when(questionRepository.save(questionTableModel2Save)).thenReturn(QuestionTableModel.builder().id(2L).build());
+		
+		
+		underTest.saveQuestion(1L, Question.builder().id(-1L).order(-1).questionName("questionName").questionType(QuestionType.FIVE_SMILEYS).build());
+	}
+	
+	/**
+	 * Test method for
+	 * {@link org.asck.service.impl.FeedbackServiceImpl#saveQuestion(java.lang.Long, org.asck.service.model.Question)}.
+	 */
+	@Test
+	public void testSaveQuestion_OneQuestionsExistsQuestionToSaveHaveOrderGreaterThenMaxQuestionSize_SaveQuestionWithOrder2() throws Exception {
+		
+		when(questionRepository.findAllByEventIdOrderByOrder(1L)).thenReturn(Arrays.asList(QuestionTableModel.builder().id(1L).eventId(1L).order(1).questionTitle("questionName").questionTypeId(QuestionType.FIVE_SMILEYS.getDbId()).build()));
+		QuestionTableModel questionTableModel2Save = QuestionTableModel.builder().id(-1L).eventId(1L).order(2).questionTitle("questionName").questionTypeId(QuestionType.FIVE_SMILEYS.getDbId()).build();
+		when(questionRepository.save(questionTableModel2Save)).thenReturn(QuestionTableModel.builder().id(2L).build());
+		
+		
+		underTest.saveQuestion(1L, Question.builder().id(-1L).order(3).questionName("questionName").questionType(QuestionType.FIVE_SMILEYS).build());
+	}
+	
+	/**
+	 * Test method for
+	 * {@link org.asck.service.impl.FeedbackServiceImpl#saveQuestion(java.lang.Long, org.asck.service.model.Question)}.
+	 */
+	@Test
+	public void testSaveQuestion_TwoQuestionsExistsAndQuestionToSaveHaveOrderBetweenBoth_SaveQuestionWithOrder2AndShiftExistingOneToOrder3AndSaveOnDatabase() throws Exception {
+		
+		QuestionTableModel question1 = QuestionTableModel.builder().id(1L).eventId(1L).order(1).questionTitle("question1").questionTypeId(QuestionType.FIVE_SMILEYS.getDbId()).build();
+		QuestionTableModel question2 = QuestionTableModel.builder().id(2L).eventId(1L).order(2).questionTitle("question2").questionTypeId(QuestionType.FIVE_SMILEYS.getDbId()).build();
+		QuestionTableModel question2WithNewOrder = QuestionTableModel.builder().id(2L).eventId(1L).order(3).questionTitle("question2").questionTypeId(QuestionType.FIVE_SMILEYS.getDbId()).build();
+		
+		when(questionRepository.findAllByEventIdOrderByOrder(1L)).thenReturn(Arrays.asList(question1, question2));
+		QuestionTableModel questionTableModel2Save = QuestionTableModel.builder().id(-1L).eventId(1L).order(2).questionTitle("questionName").questionTypeId(QuestionType.FIVE_SMILEYS.getDbId()).build();
+		
+		when(questionRepository.save(question2WithNewOrder)).thenReturn(question2WithNewOrder);
+		
 		when(questionRepository.save(questionTableModel2Save)).thenReturn(QuestionTableModel.builder().id(2L).build());
 		
 		
