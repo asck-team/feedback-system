61ff1dbccb455a34b87c27d24402be683189644b
Constantin Krüger <Constantinidis@web.de>
2019-01-03 15:52:32 +0100

    add link to signup page

--- ui-thymeleaf/src/main/java/org/asck/config/SecurityConfig.java
+++ ui-thymeleaf/src/main/java/org/asck/config/SecurityConfig.java
@@ -18,7 +18,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/img/**", "/css/**", "/webjars/**").permitAll().anyRequest().authenticated().and().formLogin().loginPage("/login").permitAll().and().logout().permitAll();
+		http.authorizeRequests().antMatchers("/img/**", "/css/**", "/webjars/**", "/signup").permitAll().anyRequest().authenticated().and().formLogin().loginPage("/login").permitAll().and().logout().permitAll();
 	}
 
 }
