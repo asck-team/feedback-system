3e8bbdc5de31eff8f44cdd22948f1327fa2b6d49
Andrei Sava <andrei.alexandru.sava@gmail.com>
2019-01-06 12:11:08 +0100

    fix security problem when restAPI was called(post, put)
    - change deprecated Encoder

--- src/main/java/org/asck/config/SecurityConfig.java
+++ src/main/java/org/asck/config/SecurityConfig.java
@@ -2,12 +2,17 @@ package org.asck.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+@Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
@@ -18,14 +23,34 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
 		if (securityEnabled) {
 			auth.inMemoryAuthentication().withUser(
-					User.withDefaultPasswordEncoder().username("user").password("password").roles("USER").build());
+					User.withUsername("user").password(encoder().encode("password")).roles("USER").build());
 		}
 	}
+	
+	@Bean
+	public PasswordEncoder  encoder() {
+	    return new BCryptPasswordEncoder();
+	}
+
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		if (securityEnabled) {
-			http.authorizeRequests().antMatchers("/img/**", "/css/**", "/webjars/**", "/signup", "swagger-ui.html" ,"/v1/feedback/**", "/answer").permitAll().anyRequest().authenticated().and().formLogin().loginPage("/login").permitAll().and().logout().permitAll();
+			http.csrf().disable()
+            .authorizeRequests()
+            .and()
+            .exceptionHandling()
+            .and()
+            .authorizeRequests()
+            .antMatchers("/img/**", "/css/**", "/webjars/**", "/signup", "swagger-ui.html" ,"/v1/feedback/**", "/answer")
+			.permitAll().anyRequest().authenticated().and()
+			.formLogin().loginPage("/login").permitAll().and()
+			.logout().permitAll()
+            .and()
+            .httpBasic()
+            .and()
+            .logout();
+			
 		} else {
 			http.authorizeRequests().antMatchers("/**").permitAll().and().csrf().disable();
 		}
